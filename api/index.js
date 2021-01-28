2f94cf99b8e41b97224b2e9a7d836ea699c3ad39
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2021-01-28 10:18:50 +0100

    fix vercel configuration

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.render('index', {keywords: references.randomizeKeywords()});
 });
 
